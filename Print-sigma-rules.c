20f3e2ed3553b3b828a94ec7cca3c6103f453ee3
Md Mahin Hossain <190971278+mdmahinhossain793@users.noreply.github.com>
2024-12-12 23:56:25 +0600

    Add files via upload

---
+++ Print-sigma-rules.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+  
+printf("best beautiful sigma male rules");
+  
+printf("sigma rule of gitmen #2547 - :   please connect");
+printf("sigma rule of gitmen #2016 - :   Take Full Responsibility");
+
+printf("sigma rule of introvertmen #0158 - :   Be calm");
+printf("sigma rule of introvertmen #2016 - :   you are my bird "); // hahahahahahahha
+
+printf("sigma rule number #0158  for gama men  - :");
+printf("sigma rule number #1106  for gama men  - :");
+printf("sigma rule number #2016  for gama men  - :");
+printf("sigma rule number #m1106 for gama men  - :");
+
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha 
+
+printf("sigma rule number #0021  for motherfucker  - :"); //hahahahaha
+printf("sigma rule number #0078  for motherfucker  - :"); //hahahahaha
+
+  return 0;
+}
